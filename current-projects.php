4ccd4a34997e6b8e8fe9eff6dc7de719f0262262
HarryMcKinney <harrypmckinney@gmail.com>
2016-02-28 16:29:47 -0500

    additional site files

---
+++ current-projects.php
@@ -0,0 +1,169 @@
+
+<!DOCTYPE HTML>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+	<meta name="description" content="Top Level Masonry is North Dakota and Montana's Premier Brick Masonry Company"/>
+	<meta content="Twisted stacked brick is our speciality, Block Masonry, Brick Masonry, Footings, Custom Brick Work, Custom Block Work, Williston, Watford City, Dickinson North Dakota & North East Montana" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<meta name="msvalidate.01" content="1012B4DA3ECC7F603E0E8132F7ABCF90" />
+
+	<link rel="icon" type="image/ico" href="img/toplevel.ico"/>
+
+    <title>Top Level Masonry llc. North Dakota | North East Montana</title>
+
+    <link rel="stylesheet" href="css/foundation.css" />
+	<script src="js/vendor/modernizr.js"></script>
+
+				<script>
+				var message = "function disabled copyright T.L.M. LLC";
+				function rtclickcheck(keyp){ if (navigator.appName == "Netscape" && keyp.which == 3){ alert(message); return false; }
+				if (navigator.appVersion.indexOf("MSIE") != -1 && event.button == 2) { 	alert(message); return false; } }
+				document.onmousedown = rtclickcheck;
+				</script>
+
+				<!-- SMOOTH SCROLL -->
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script>
+	$(function() {
+	$('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+		}
+		}
+	});
+	});
+	</script>
+	<!-- End of SMOOTH SCROLL -->
+
+				 <!--[if lt IE 9]>
+					<style>
+						header
+						{
+						margin: 0 auto 20px auto;
+						}
+						#four_columns .img-item figure span.thumb-screen
+						{
+						display:none;
+						}
+					</style>
+				<![endif] -->
+
+  </head>
+  <body>
+	<div id="outside_loop">
+    <div id="top">
+    <div class="row">
+    <div class="large-3 columns">
+      <h1><img src="img/top_level_masonry_image_logo.png" alt="Top Level Masonry logo" title="Top Level Masonry logo"/></h1>
+    </div>
+    <div class="large-9 columns">
+      <ul class="right button-group">
+	  <li><a href="index.php" class="button">Home</a></li>
+      <li><a href="contact.php" class="button">Contact Us</a></li>
+      <li><a href="articles.php" class="button">Articles</a></li>
+      </ul>
+     </div>
+   </div>
+   </div>
+
+  <div class="row">
+    <div class="large-12 columns">
+
+    <div id="slider">
+	<div class="top_level_slider">
+     <div> <img src="img/top_level_masonry_image_slider.png" alt="Top Level Masonry specializes in many different types of brick and block applications" title="Top Level Masonry specializes in many different types of brick and block applications"/> </div>
+	 </div>
+    </div>
+    <hr/>
+    </div>
+  </div>
+
+  <!--begin new code-->
+
+  <div class="row">
+        <div class="large-4 columns">
+          <img src="img/toplevel_march_2015_photo_1.png" alt="photo of a Top Level Masonry Brick Custom Twisted Column" title="Custom Twisted Brick Work is our Speciality">
+        </div>
+        <div class="large-8 columns">
+          <h4>Custom Brick Work is our Speciality</h4>
+          <div class="row">
+            <div class="large-6 columns">
+              <p>One of the hottest building trends in brick work today is the twisted brick or twisted column design. We are masters at this, we have many years of experience in this style and we know what it takes to pull it off correctly, this is why we can guarantee a quality column for longer than the life of your home.</p>
+            </div>
+            <div class="large-6 columns">
+              <p>Mastering these types of columns takes patience and discipline and these are things that you won't find in every mason, that is another reason to choose Top Level Masonry for your next project! <a href="contact.php">Contact</a> us today for a quote on any custom brick work!</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+
+
+      <div class="row">
+        <div class="large-8 columns">
+          <h4>Perfect Brick Work every time</h4>
+
+          <p>At Top Level Masonry we know exactly what it takes to build a home or any structure out of brick, we have many years of experience and we guarantee the work that we do. We are 100% committed to making sure the brick work that we deliver will stand the test of time!</p>
+
+          <p>Have a project in mind? Regardless if it is new construction, a remodel or an addition - please <a href="contact">contact us today</a> for a quote and our advice on your project. We are currently working in the Williston North Dakota area; around (and in) Watford City as well as Dickinson North Dakota and North East Montana. Contact us today so we can get started on your next project!</p>
+
+        </div>
+        <div class="large-4 columns">
+          <img src="img/toplevel_march_2015_photo_2.png" alt="This is a photo of a two story brick side of a house that we just completed" title="No Job Too Small for Top Level Masonry">
+        </div>
+      </div>
+
+	  <hr>
+
+
+  <!--end new code-->
+
+  <div class="row">
+    <div class="large-12 columns">
+    <div id="slider">
+	<div class="top_level_slider">
+     <div> <img src="img/top_level_masonry_image_footer.png" alt="Top Level Masonry image of brick and block work" title="Top Level Masonry specializes in many different types of brick and block work"/> </div>
+	 </div>
+    </div>
+
+    <hr/>
+    </div>
+  </div>
+
+  <footer class="row">
+    <div class="large-12 columns">
+      <div class="row">
+        <div class="large-6 columns">
+          <p>&copy; <?php echo date('Y')?> Top Level Masonry LLC.</p>
+        </div>
+        <div class="large-6 columns">
+          <ul class="inline-list right">
+			<li><a href="#top"><p style="color:black;">back to the top</p></a></li>
+			<li><a href="contact.php"><p style="color:black;">contact us</p></a></li>
+			<li><a href="tel:7017135610"><p style="color:black">Call us at <mark>701-713-5610</mark></p></a></li>
+			<li><a href="http://www.facebook.com/toplevelmasonry"><img src="img/facebook_logo_top_level.png" alt="like us on facebook" title="like Top Level on facebook"></a></li>
+			<li><a href="articles.php"><p style="color:black;">articles</p></a></li>
+			<li><a href="index.php"><p style="color:black;">home</p></a></li>
+			<li><a href="sitemap.xml"><p style="color:black;">xml sitemap</p></a></li>
+            <li><a href="http://www.4point0designs.com/index.php"><p style="color:black;">web page by 4.0</p></a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </footer>
+  </div><!--end outside loop div-->
+
+   <script src="js/vendor/jquery.js"></script>
+   <script src="js/foundation.min.js"></script>
+   <script>$(document).foundation();</script>
+  </body>
+
+</html>
